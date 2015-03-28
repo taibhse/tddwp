667fff56704cac92c3e5f6abf3541f5374a78e55
Chris Walsh <cwalsh@newrelic.com>
2015-03-28 16:44:54 +0000

    Redirect after POST, shows all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -33,21 +33,21 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         #User types feathers into the text box and hits enter
-        inputbox.send_keys('Buy feathers')
+        inputbox.send_keys('1: Buy feathers')
         inputbox.send_keys(Keys.ENTER)
 
         #The page updates and lists "1: Buy feathers" as a to-do item
-        self.check_for_row_in_list_table('Buy feathers')
+        self.check_for_row_in_list_table('1: Buy feathers')
 
         #There is still a text box inviting us to add another item. 
         #We enter use feathers to make a fly.
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use feathers to make a fly')
+        inputbox.send_keys('2: Use feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         #The Page updates again, and now shows both items on the list:
-        self.check_for_row_in_list_table('Buy feathers')
-        self.check_for_row_in_list_table('Use feathers to mak a fly')
+        self.check_for_row_in_list_table('1: Buy feathers')
+        self.check_for_row_in_list_table('2: Use feathers to mak a fly')
 
         #does it remember the list?
         #the site generates a random URL
